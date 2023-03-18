0a8b0b138844dfe621ddf11b329512a16294e23b
Lee Harrold <lhharrold@sep.com>
2023-03-18 10:33:48 -0400

    Save recording as new wav

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+import json
 import tempfile
 import flask
 from flask import request
@@ -7,6 +9,69 @@ import whisper
 
 app = flask.Flask(__name__)
 CORS(app)
+
+
+@app.route('/load_project', methods=['POST'])
+def load_project():
+    if request.method == 'POST':
+        return
+
+
+@app.route('/start_project', methods=['POST'])
+def start_project():
+    if request.method == 'POST':
+        # make wavs dir
+        try:
+            pathlib.Path('wavs').mkdir(exist_ok=False)
+        except FileExistsError:
+            return 'Project already exists. This version supports only one project.'
+        except:
+            return 'Some other error'
+
+        config = {
+            "file_count": 0
+        }
+        # make config file
+        with open('wavs/config.txt', 'w') as f:
+            f.write(json.dumps(config))
+        
+        with open('wavs/metadata.txt', 'w') as f:
+            f.write('empty')
+
+        return f'Created project at {pathlib.Path.cwd()}/wavs'
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    if request.method == 'POST':
+        language = request.form['language']
+        model = request.form['model_size']
+
+        # there are no english models for large
+        if model != 'large' and language == 'english':
+            model = model + '.en'
+        audio_model = whisper.load_model(model)
+
+        wav_file = request.files['audio_data']
+        save_path = f'wavs/{next_file_name()}'
+        wav_file.save(save_path)
+
+        if language == 'english':
+            result = audio_model.transcribe(save_path, language='english')
+        else:
+            result = audio_model.transcribe(save_path)
+
+        return {"text": result['text'], "filename": save_path} 
+
+
+@app.route('/updateTranscription', methods=['PUT'])
+def updateTranscription():
+    if request.method == 'PUT':
+        # get filename from request
+        # get new text from request
+        # use filename to find line to update
+        # replace old text in that line with new text
+        return
 
 
 @app.route('/transcribe', methods=['POST'])
@@ -19,7 +84,7 @@ def transcribe():
         if model != 'large' and language == 'english':
             model = model + '.en'
         audio_model = whisper.load_model(model)
-
+            
         temp_dir = tempfile.mkdtemp()
         save_path = os.path.join(temp_dir, 'temp.wav')
 
@@ -34,3 +99,14 @@ def transcribe():
         return result['text']
     else:
         return "This endpoint only processes POST wav blob"
+        
+
+def add_line(text, filename): 
+    with open('wavs/metadata.txt', 'a') as f:
+        f.write(f'{filename}|{text}')
+
+
+def next_file_name():
+    with open('wavs/config.txt', 'r') as f:
+        config = json.load(f)
+    return f'wav{config["file_count"]}'
--- interface/src/App.js
+++ interface/src/App.js
@@ -2,51 +2,55 @@ import React, { useState, useEffect, useRef } from "react";
 import { Button } from "react-bootstrap";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TranscribeOutput from "./TranscribeOutput";
 import SettingsSections from "./SettingsSection";
-import { ReactMic } from 'react-mic';
+import { ReactMic } from "react-mic";
 import axios from "axios";
 import { PulseLoader } from "react-spinners";
 
 const useStyles = () => ({
   root: {
-    display: 'flex',
-    flex: '1',
-    margin: '100px 0px 100px 0px',
-    alignItems: 'center',
-    textAlign: 'center',
-    flexDirection: 'column',
+    display: "flex",
+    flex: "1",
+    margin: "100px 0px 100px 0px",
+    alignItems: "center",
+    textAlign: "center",
+    flexDirection: "column",
   },
   title: {
-    marginBottom: '30px',
+    marginBottom: "30px",
   },
   settingsSection: {
-    marginBottom: '20px',
-    display: 'flex',
-    width: '100%',
+    marginBottom: "20px",
+    display: "flex",
+    width: "100%",
   },
   buttonsSection: {
-    marginBottom: '40px',
+    marginBottom: "40px",
   },
   recordIllustration: {
-    width: '100px',
-  }
+    width: "100px",
+  },
 });
+
+const BACKEND_URL = "http://0.0.0.0:8000";
 
 const App = ({ classes }) => {
   const [transcribedData, setTranscribedData] = useState([]);
-  const [interimTranscribedData, ] = useState('');
+  const [filename, setFilename] = useState("");
+  const [interimTranscribedData] = useState("");
   const [isRecording, setIsRecording] = useState(false);
   const [isTranscribing, setIsTranscribing] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState('english');
+  const [selectedLanguage, setSelectedLanguage] = useState("english");
   const [selectedModel, setSelectedModel] = useState(1);
   const [transcribeTimeout, setTranscribeTimout] = useState(5);
-  const [stopTranscriptionSession, setStopTranscriptionSession] = useState(false);  
+  const [stopTranscriptionSession, setStopTranscriptionSession] =
+    useState(false);
 
   const intervalRef = useRef(null);
-  
+
   const stopTranscriptionSessionRef = useRef(stopTranscriptionSession);
   stopTranscriptionSessionRef.current = stopTranscriptionSession;
 
@@ -56,30 +60,132 @@ const App = ({ classes }) => {
   const selectedModelRef = useRef(selectedModel);
   selectedModelRef.current = selectedModel;
 
-  const supportedLanguages = ['english', 'chinese', 'german', 'spanish', 'russian', 'korean', 'french', 'japanese', 'portuguese', 'turkish', 'polish', 'catalan', 'dutch', 'arabic', 'swedish', 'italian', 'indonesian', 'hindi', 'finnish', 'vietnamese', 'hebrew', 'ukrainian', 'greek', 'malay', 'czech', 'romanian', 'danish', 'hungarian', 'tamil', 'norwegian', 'thai', 'urdu', 'croatian', 'bulgarian', 'lithuanian', 'latin', 'maori', 'malayalam', 'welsh', 'slovak', 'telugu', 'persian', 'latvian', 'bengali', 'serbian', 'azerbaijani', 'slovenian', 'kannada', 'estonian', 'macedonian', 'breton', 'basque', 'icelandic', 'armenian', 'nepali', 'mongolian', 'bosnian', 'kazakh', 'albanian', 'swahili', 'galician', 'marathi', 'punjabi', 'sinhala', 'khmer', 'shona', 'yoruba', 'somali', 'afrikaans', 'occitan', 'georgian', 'belarusian', 'tajik', 'sindhi', 'gujarati', 'amharic', 'yiddish', 'lao', 'uzbek', 'faroese', 'haitian creole', 'pashto', 'turkmen', 'nynorsk', 'maltese', 'sanskrit', 'luxembourgish', 'myanmar', 'tibetan', 'tagalog', 'malagasy', 'assamese', 'tatar', 'hawaiian', 'lingala', 'hausa', 'bashkir', 'javanese', 'sundanese']
+  const supportedLanguages = [
+    "english",
+    "chinese",
+    "german",
+    "spanish",
+    "russian",
+    "korean",
+    "french",
+    "japanese",
+    "portuguese",
+    "turkish",
+    "polish",
+    "catalan",
+    "dutch",
+    "arabic",
+    "swedish",
+    "italian",
+    "indonesian",
+    "hindi",
+    "finnish",
+    "vietnamese",
+    "hebrew",
+    "ukrainian",
+    "greek",
+    "malay",
+    "czech",
+    "romanian",
+    "danish",
+    "hungarian",
+    "tamil",
+    "norwegian",
+    "thai",
+    "urdu",
+    "croatian",
+    "bulgarian",
+    "lithuanian",
+    "latin",
+    "maori",
+    "malayalam",
+    "welsh",
+    "slovak",
+    "telugu",
+    "persian",
+    "latvian",
+    "bengali",
+    "serbian",
+    "azerbaijani",
+    "slovenian",
+    "kannada",
+    "estonian",
+    "macedonian",
+    "breton",
+    "basque",
+    "icelandic",
+    "armenian",
+    "nepali",
+    "mongolian",
+    "bosnian",
+    "kazakh",
+    "albanian",
+    "swahili",
+    "galician",
+    "marathi",
+    "punjabi",
+    "sinhala",
+    "khmer",
+    "shona",
+    "yoruba",
+    "somali",
+    "afrikaans",
+    "occitan",
+    "georgian",
+    "belarusian",
+    "tajik",
+    "sindhi",
+    "gujarati",
+    "amharic",
+    "yiddish",
+    "lao",
+    "uzbek",
+    "faroese",
+    "haitian creole",
+    "pashto",
+    "turkmen",
+    "nynorsk",
+    "maltese",
+    "sanskrit",
+    "luxembourgish",
+    "myanmar",
+    "tibetan",
+    "tagalog",
+    "malagasy",
+    "assamese",
+    "tatar",
+    "hawaiian",
+    "lingala",
+    "hausa",
+    "bashkir",
+    "javanese",
+    "sundanese",
+  ];
 
-  const modelOptions = ['tiny', 'base', 'small', 'medium', 'large', 'large-v1']
+  const modelOptions = ["tiny", "base", "small", "medium", "large", "large-v1"];
 
   useEffect(() => {
     return () => clearInterval(intervalRef.current);
   }, []);
 
-
   function handleTranscribeTimeoutChange(newTimeout) {
-    setTranscribeTimout(newTimeout)
+    setTranscribeTimout(newTimeout);
   }
 
   function startRecording() {
-    setStopTranscriptionSession(false)
-    setIsRecording(true)
-    intervalRef.current = setInterval(transcribeInterim, transcribeTimeout * 1000)
+    setStopTranscriptionSession(false);
+    setIsRecording(true);
+    intervalRef.current = setInterval(
+      transcribeInterim,
+      transcribeTimeout * 1000
+    );
   }
 
   function stopRecording() {
     clearInterval(intervalRef.current);
-    setStopTranscriptionSession(true)
-    setIsRecording(false)
-    setIsTranscribing(false)
+    setStopTranscriptionSession(true);
+    setIsRecording(false);
+    setIsTranscribing(false);
   }
 
   function onData(recordedBlob) {
@@ -87,13 +193,13 @@ const App = ({ classes }) => {
   }
 
   function onStop(recordedBlob) {
-    transcribeRecording(recordedBlob)
-    setIsTranscribing(true)  
+    saveRecordingAndKeepPath(recordedBlob);
+    setIsTranscribing(true);
   }
 
   function transcribeInterim() {
     clearInterval(intervalRef.current);
-    setIsRecording(false)
+    setIsRecording(false);
   }
 
   function transcribeRecording(recordedBlob) {
@@ -101,49 +207,114 @@ const App = ({ classes }) => {
       "content-type": "multipart/form-data",
     };
     const formData = new FormData();
-    formData.append("language", selectedLangRef.current)
-    formData.append("model_size", modelOptions[selectedModelRef.current])
-    formData.append("audio_data", recordedBlob.blob, 'temp_recording');
-    axios.post("http://0.0.0.0:8000/transcribe", formData, { headers })
+    formData.append("language", selectedLangRef.current);
+    formData.append("model_size", modelOptions[selectedModelRef.current]);
+    formData.append("audio_data", recordedBlob.blob, "temp_recording");
+    axios
+      .post(`${BACKEND_URL}/transcribe`, formData, { headers })
       .then((res) => {
-        setTranscribedData(oldData => [...oldData, res.data])
-        setIsTranscribing(false)
-        intervalRef.current = setInterval(transcribeInterim, transcribeTimeout * 1000)
+        setTranscribedData((oldData) => [...oldData, res.data]);
+        setIsTranscribing(false);
+        intervalRef.current = setInterval(
+          transcribeInterim,
+          transcribeTimeout * 1000
+        );
       });
-      
-      if (!stopTranscriptionSessionRef.current){
-        setIsRecording(true)    
-      }
+
+    if (!stopTranscriptionSessionRef.current) {
+      setIsRecording(true);
+    }
+  }
+
+  function saveRecordingAndKeepPath(recordedBlob) {
+    const headers = {
+      "content-type": "multipart/form-data",
+    };
+    const formData = new FormData();
+    formData.append("language", selectedLangRef.current);
+    formData.append("model_size", modelOptions[selectedModelRef.current]);
+    formData.append("audio_data", recordedBlob.blob, "temp_recording");
+    axios.post(`${BACKEND_URL}/save`, formData, { headers }).then((res) => {
+      const { text, filename } = res.data;
+      setFilename(() => filename);
+      setTranscribedData(() => [text]);
+      setIsTranscribing(false);
+      intervalRef.current = setInterval(
+        transcribeInterim,
+        transcribeTimeout * 1000
+      );
+    });
+
+    if (!stopTranscriptionSessionRef.current) {
+      setIsRecording(true);
+    }
   }
 
   return (
     <div className={classes.root}>
       <div className={classes.title}>
         <Typography variant="h3">
-          Whisper Playground <span role="img" aria-label="microphone-emoji">🎤</span>
+          Whisper Playground{" "}
+          <span role="img" aria-label="microphone-emoji">
+            🎤
+          </span>
         </Typography>
       </div>
       <div className={classes.settingsSection}>
-        <SettingsSections disabled={isTranscribing || isRecording} possibleLanguages={supportedLanguages} selectedLanguage={selectedLanguage}
-          onLanguageChange={setSelectedLanguage} modelOptions={modelOptions} selectedModel={selectedModel} onModelChange={setSelectedModel}
-          transcribeTimeout={transcribeTimeout} onTranscribeTiemoutChanged={handleTranscribeTimeoutChange} />
+        <SettingsSections
+          disabled={isTranscribing || isRecording}
+          possibleLanguages={supportedLanguages}
+          selectedLanguage={selectedLanguage}
+          onLanguageChange={setSelectedLanguage}
+          modelOptions={modelOptions}
+          selectedModel={selectedModel}
+          onModelChange={setSelectedModel}
+          transcribeTimeout={transcribeTimeout}
+          onTranscribeTiemoutChanged={handleTranscribeTimeoutChange}
+        />
       </div>
-      <div className={classes.buttonsSection} >
-        {!isRecording && !isTranscribing && <Button onClick={startRecording} variant="primary">Start transcribing</Button>}
-        {(isRecording || isTranscribing) && <Button onClick={stopRecording} variant="danger" disabled={stopTranscriptionSessionRef.current}>Stop</Button>}
+      <div className={classes.buttonsSection}>
+        {!isRecording && !isTranscribing && (
+          <Button onClick={startRecording} variant="primary">
+            Start transcribing
+          </Button>
+        )}
+        {(isRecording || isTranscribing) && (
+          <Button
+            onClick={stopRecording}
+            variant="danger"
+            disabled={stopTranscriptionSessionRef.current}
+          >
+            Stop
+          </Button>
+        )}
       </div>
 
       <div className="recordIllustration">
-        <ReactMic record={isRecording} className="sound-wave" onStop={onStop}
-          onData={onData} strokeColor="#0d6efd" backgroundColor="#f6f6ef" />
+        <ReactMic
+          record={isRecording}
+          className="sound-wave"
+          onStop={onStop}
+          onData={onData}
+          strokeColor="#0d6efd"
+          backgroundColor="#f6f6ef"
+        />
       </div>
 
       <div>
-        <TranscribeOutput transcribedText={transcribedData} interimTranscribedText={interimTranscribedData} />
-        <PulseLoader sizeUnit={"px"} size={20} color="purple" loading={isTranscribing} />
+        <TranscribeOutput
+          transcribedText={transcribedData}
+          interimTranscribedText={interimTranscribedData}
+        />
+        <PulseLoader
+          sizeUnit={"px"}
+          size={20}
+          color="purple"
+          loading={isTranscribing}
+        />
       </div>
     </div>
   );
-}
+};
 
 export default withStyles(useStyles)(App);
